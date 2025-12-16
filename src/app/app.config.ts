28ff5090827b42c0a3408ff3cff2ed5ee238700b
saujanyamahajan <saujanya.9919@gmail.com>
2025-12-16 18:00:02 +0530

    init: Enable HTTP Calls

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+//Enable HTTP Calls
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
