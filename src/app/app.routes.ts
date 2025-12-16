fac6b54f4f4cd690da1fc3162fa99146abc0c9d7
saujanyamahajan <saujanya.9919@gmail.com>
2025-12-16 18:57:40 +0530

    init: updated empoloye list ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+// import { EmployeeAddComponent } from './components/employee-add/employee-add.component';
+// import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'employees', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+//   { path: 'add', component: EmployeeAddComponent },
+//   { path: 'edit/:id', component: EmployeeEditComponent }
+];
--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../../models/employee.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { EmployeeService } from '../../services/employee.service';
+import { Employee } from '../../models/employee.model';
 
 @Component({
   selector: 'app-employee-list',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './employee-list.component.html'
 })
 export class EmployeeListComponent implements OnInit {
@@ -13,8 +17,12 @@ export class EmployeeListComponent implements OnInit {
   constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
-    this.employeeService.getAll().subscribe(data => {
-      this.employees = data;
+    this.employeeService.getAll().subscribe({
+      next: (data) => {
+        this.employees = data;
+        console.log(this.employees); // 🔍 check in console
+      },
+      error: (err) => console.error(err)
     });
   }
 }
